8d9d000a13e2cfe367107664abd013776d509682
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2019-12-14 18:18:17 +0300

    * Removed an unnecessary class MainLayout * Change button's text in sidebar * Added label with title on first and second screen

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+#  -*- coding: utf-8 -*-
+
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.lang.builder import Builder
@@ -5,7 +7,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.properties import ObjectProperty
 from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 
 Window.clearcolor = (.98, .98 ,.98, 1)
 
@@ -23,22 +24,19 @@ class MainScreen(Screen):
 class Menu(BoxLayout):
     pass
 
-class MainLayout(BoxLayout):
-    pass
-
 class AnotherScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.name = "second"
 
 class ScreenManagement(ScreenManager):
     pass
-
-presentation = Builder.load_file("main.kv")
 
 class MainApp(App):
     def build(self):
         return presentation
 
 if __name__ == "__main__":
+
+    presentation = Builder.load_file("main.kv")
+
     MainApp().run()

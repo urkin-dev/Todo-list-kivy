3350f89114a8bf26ae69b88d65b38e49a699f737
Ivan Yurkin <yurkin.ivan5456@gmail.com>
2019-12-21 22:03:58 +0300

    * All kivy modules were moved to one kv file * Widget task init in kv file now * Button Edit removed, now this is icon * Added checkbox to add task to favorite

--- index.py
+++ index.py
@@ -3,66 +3,56 @@
 from kivymd.app import MDApp
 
 from kivy.core.window import Window
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.properties import ObjectProperty
 from kivy.clock import Clock
 
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.checkbox import CheckBox
-
 from kivymd.uix.picker import MDDatePicker
+from kivy.uix.image import Image
 
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 
 Window.clearcolor = (.98, .98 ,.98, 1)
-Window.size = (900, 600)
 
 
 class Task(BoxLayout):
+
+    lb = ObjectProperty()
+    editBtn = ObjectProperty()
+    favorite = ObjectProperty()
+    imageEdit = ObjectProperty()
+
     def __init__(self, text):
         super().__init__()
 
         self.text = text
-
-        self.orientation = "horizontal"
-        self.size_hint = (1, None)
-        self.height = 60
-
-        self.lb = Label(text = self.text, color = (0, 0, 0, 1), size_hint = (None, None), size  = (390, 60), halign = "left", valign = "middle")
-        self.lb.text_size = (390, 60)
+        self.lb.text = self.text
 
         self.func_1 = lambda x: self.edit_task()
         self.func_2 = lambda x: self.save_task()
 
-        self.editButton = Button(text = "Edit", size_hint = (None, None), width = 85, height = 30, pos_hint = {"x": 0, "y": .3})
-
-        self.editButton.bind(on_press = self.func_1)
-
-        self.favorite = CheckBox(size_hint = (None, None), size = (60, 30), pos_hint = {"x": 1, "y": .33})
-        self.favorite.background_checkbox_normal = "./image/star(no active).png"
-        self.favorite.background_checkbox_down = "./image/star(active).png"
         self.favorite.bind(active = self.add_to_favorites)
 
-        self.add_widget(self.lb)
-        self.add_widget(self.editButton)
-        self.add_widget(self.favorite)
+        self.editBtn.bind(on_press = self.func_1)
 
     def edit_task(self):
         self.clear_widgets()
-        self.editInput = TextInput(text = self.lb.text, size_hint = (None, None), size = (390, 30), pos_hint = {"x": 0, "y": .3})
+        self.editInput = TextInput(text = self.lb.text, size_hint = (1, None), height = 30, pos_hint = {"x": 0, "y": .3})
 
-        self.editButton.text = "Save"
+        self.editBtn.text = "Save"
+        self.imageEdit.source = "./image/save.png"
 
-        self.editButton.unbind(on_press = self.func_1)
-        self.editButton.bind(on_press = self.func_2)
+        self.editBtn.unbind(on_press = self.func_1)
+        self.editBtn.bind(on_press = self.func_2)
 
         self.add_widget(self.editInput)
-        self.add_widget(self.editButton)
+        self.add_widget(self.editBtn)
         self.add_widget(self.favorite)
 
     def save_task(self):
@@ -74,32 +64,22 @@ class Task(BoxLayout):
 
         self.lb.text = self.editInput.text
 
-        self.editButton.text = "Edit"
+        self.editBtn.text = "Edit"
+        self.imageEdit.source = "./image/edit.png"
 
-        self.editButton.unbind(on_press = self.func_2)
-        self.editButton.bind(on_press = self.func_1)
+        self.editBtn.unbind(on_press = self.func_2)
+        self.editBtn.bind(on_press = self.func_1)
 
         self.add_widget(self.lb)
-        self.add_widget(self.editButton)
+        self.add_widget(self.editBtn)
         self.add_widget(self.favorite)
 
     def add_to_favorites(self, checkbox, value):
         print(value)
 
-# SideBar
-class Menu(BoxLayout):
-    pass
-
 class MainScreen(Screen):
     input = ObjectProperty()
     box = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        Clock.schedule_once(self._finish_init)
-
-    def _finish_init(self, dt):
-        self.box.add_widget(Task("Случайная задача"))
 
     def add_task(self):
         if (self.input.text == ""):
@@ -112,7 +92,8 @@ class MainScreen(Screen):
         picker.open()
 
     def get_date(self, dt):
-        print(dt)
+        self.dt = dt
+        print(self.dt)
 
 class Today(Screen):
     input = ObjectProperty()
@@ -168,10 +149,7 @@ class ScreenManagement(ScreenManager):
 
 class MainApp(MDApp):
     def build(self):
-        return presentation
+        pass
 
 if __name__ == "__main__":
-
-    presentation = Builder.load_file("main.kv")
-
     MainApp().run()
